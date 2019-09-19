5bf995557031e25e29cbf81330b6e2af54e77ade
Nick Shields <nickshields@v1040-wn-rt-b-71-94.campus-dynamic.uwaterloo.ca>
2019-09-18 20:21:06 -0400

    Updated Question 1

--- Lab 1/question1.py
+++ Lab 1/question1.py
@@ -1,24 +1,40 @@
 import random
 import math
 
-L = 75
-s = 1000
-sum = 0
-values = []
+#For a FIFO, we will use a python list.
+#ie:
+#x = []
+#x.append(3) <- enqueue
+#x.pop(0) <- dequeue
 
-for i in range(s):
-    U = random.random()
-    x = -(1/L) * math.log(1 - U)
-    values.append(x)
-    print(i, ": ", x)
-    sum += x
+class Simulator(object):
 
-average = sum/s
-print("average: ", average, 1/L)
+    def __init__(self):
+        pass
 
-varianceNum = 0
-for value in values:
-    varianceNum += (value - average)**2
-variance = varianceNum/(s-1)
+    def generateVariables(self,):
+        L = 75
+        s = 1000
+        sum = 0
+        values = []
 
-print("variance: ", variance)
+        for i in range(s):
+            U = random.random()
+            x = -(1/L) * math.log(1 - U)
+            values.append(x)
+            print(i, ": ", x)
+            sum += x
+
+        average = sum/s
+        print("average: ", average, 1/L)
+
+        varianceNum = 0
+        for value in values:
+            varianceNum += (value - average)**2
+        variance = varianceNum/(s-1)
+
+        print("variance: ", variance)
+
+if __name__ == "__main__":
+    sim = Simulator()
+    sim.generateVariables()
